45f110b6f9da6b2423a54a987dfbc1a9a3a8e5b3
zst <4254@qq.com>
2020-10-30 10:35:30 +0800

    完成个人中心的开发

--- assets/js/user_avatar.js
+++ assets/js/user_avatar.js
@@ -1,5 +1,5 @@
 $(function () {
-      // 1.1 获取裁剪区域的 DOM 元素
+  // 1.1 获取裁剪区域的 DOM 元素
   var $image = $('#image')
   // 1.2 配置选项
   const options = {
@@ -11,8 +11,45 @@ $(function () {
 
   // 1.3 创建裁剪区域
   $image.cropper(options)
-  
-    $('#btnChooseImage').on('click', function () {
-         $('#file').trigger('click')
+
+
+  $('#btnChooseImage').on('click', function () {
+    $('#file').trigger('click')
+  })
+
+  $('#file').on('change', function (e) {
+    console.log(e.target.files[0]);
+    var file = e.target.files[0]
+    newImgUrl = URL.createObjectURL(file)
+    console.log(newImgUrl);
+    $image.cropper('destroy').attr('src', newImgUrl)
+      .cropper(options)
+  })
+
+  $('#btnUpload').on('click', function () {
+    var dataURL = $image
+      .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+        width: 100,
+        height: 100 
+      })
+      .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+    $.ajax({
+      type: 'POST',
+      url: '/my/update/avatar',
+      data: {
+        avatar:dataURL
+      },
+      success: function (res) {
+        if (res.status !== 0) {
+             return    layui.layer.msg(res.message)
+        }
+        layui.layer.msg(res.message)
+        window.parent.getUserInfo()
+      }
     })
+  })
+  
+
+
+
 })
